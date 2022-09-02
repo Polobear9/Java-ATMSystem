335dadec25b47f6a0108de69ad4d4e61861b7ab6
Polobear9 <lieams1996@gmail.com>
2022-09-02 13:32:09 +0900

    All System is finish !! and it is a prototype.

---
+++ src/BankingSystem/BankMenu_CheckBalance.java
@@ -0,0 +1,29 @@
+package BankingSystem;
+
+import java.util.Scanner;
+
+public class BankMenu_CheckBalance {
+    static Scanner scanner = new Scanner(System.in);
+    private static boolean isSystem_Run = true;
+
+    public static void BankMenu_CheckBalanceDo(Userinfo userinfo) {
+        while (isSystem_Run) {
+            System.out.println("Welcome to Check Balance System.");
+
+            System.out.println("Your Balance is : " + userinfo.getBalance() + " Japan Yen.");
+            System.out.println("Your Balance is : " + userinfo.getBANK_NUMBER() + " <- that is your Banknumber.");
+
+            System.out.println("DO you want out? 1.YES 2.NO");
+            int selectMenuNum = scanner.nextInt();
+
+            if (selectMenuNum == 1) {
+                isSystem_Run = false;
+                break;
+            }
+            if (selectMenuNum == 2) {
+                isSystem_Run = true;
+            }
+        }
+
+    }
+}
---
+++ src/BankingSystem/BankMenu_DisbursementsMoney.java
@@ -0,0 +1,29 @@
+package BankingSystem;
+
+import java.util.Scanner;
+
+public class BankMenu_DisbursementsMoney {
+    static Scanner sc = new Scanner(System.in);
+    private static boolean isSystem_Run = true;
+    public static void BankMenu_DisbursementsMoneyDo(Userinfo userinfo){
+        while (isSystem_Run) {
+            System.out.println("Welcome to Out Money System.");
+            System.out.println("DO you want out of Money? : 1.YES 2.NO");
+            int SelectMenuNum = sc.nextInt();
+            if (SelectMenuNum == 1) {
+                System.out.println("How many Money do you want out? : ");
+                int Out_Money = sc.nextInt();
+                System.out.println("Do you want out -->> : " + Out_Money + " ? ");
+                System.out.println(" 1.YES 2.NO ");
+                int MenuSelect = sc.nextInt();
+                if (MenuSelect == 1) {
+                    userinfo.setBalance(userinfo.getBalance() - Out_Money);
+                    System.out.println("Your Money is left : " + userinfo.getBalance());
+                }
+            }
+            if (SelectMenuNum == 2) {
+                break;
+            }
+        }
+    }
+}
